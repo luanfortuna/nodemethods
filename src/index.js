0982eab5eca326fcf2118a38f00f6122735ede6d
Luan Fortuna <luan395@gmail.com>
2022-05-02 21:33:34 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,52 @@
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+
+
+app.get("/teams", (request, response)=> {
+
+    const query = request.query;
+
+    console.log(query);
+
+  return response.json(["Botafogo","Goiás","Santos","Redbull","Palmeiras","Chapecoense"])
+});
+
+app.post("/teams", (request, response)=> {
+
+    const body = request.body;
+    console.log(body);
+
+return response.json(["Botafogo","Goiás","Santos","Redbull","Palmeiras","Chapecoense","Atlético-MG","Coritiba"])
+});
+
+app.put("/teams/:id", (request, response)=> {
+
+    const params = request.params;
+    console.log(params);
+
+return response.json(["Botafogo","Bahia","Santos","Corinthians","Palmeiras","Náutico","Atlético-MG","Grêmio"])    
+
+});
+
+app.patch("/teams/:id", (request, response)=> {
+
+    return response.json(["Palmeiras","Náutico","Atlético-MG","Grêmio"])    
+
+});
+
+app.delete("/teams/:id", (request, response)=> {
+
+    return response.json(["Atlético-MG","Grêmio"])    
+
+});
+
+
+
+
+
+app.listen(3333);
